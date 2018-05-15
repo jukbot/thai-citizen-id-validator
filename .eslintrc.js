fdb8cf919eaac098c34aea96e0868fb2063107d3
jukbot <chukkrit.juk@gmail.com>
2018-05-16 03:22:02 +0700

    Fixed CI build :green_heart:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   root: true,
   env: {
+    'jest/globals': true,
     browser: true,
     node: true
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
+  plugins: ['jest'],
   extends: [
-    'standard'
+    'standard',
+    'plugin:jest/recommended'
   ],
   rules: {
+    'quotes': [ 2, 'single'],
     'no-console': 'off'
   }
 }
